03462305156e5bdbbd782b919e34b5313e7f607b
milad2073 <miladrahimipo@Gmail.com>
2022-08-30 10:29:58 +0430

    Forward method added The variable with name 'T' changed to 'M' and the previous variable with name 'M' deleted because was not used anywhere in the code

--- hidden_matkov_model.py
+++ hidden_matkov_model.py
@@ -3,12 +3,8 @@ from time import sleep
 import numpy as np 
 import sys
 
-N = 5
-
-
-
-M = 3
-T = 27
+N = 5 # Number of states
+M = 27 # number of all possible observations
 
 tmpA = np.random.uniform(size=[N,N])
 # tmpA = np.eye(N)
@@ -16,9 +12,9 @@ A = np.zeros_like(tmpA)
 for i in range(tmpA.shape[0]):
     for j in range(tmpA.shape[1]):
         A[i,j] = tmpA[i,j]/np.sum(tmpA[i,:])
-print(A)
+
 # np.fill_diagonal(A,0)
-tmpB = np.random.uniform(size=[N,T])
+tmpB = np.random.uniform(size=[N,M])
 B = np.zeros_like(tmpB)
 for i in range(tmpB.shape[0]):
     for j in range(tmpB.shape[1]):
@@ -47,28 +43,52 @@ def showNewRes(res):
     elif res == 26:
         print(' ',end='')
     else:
-        print(chr(res+97),end='')
+        print(chr(res+97),end='-')
     sys.stdout.flush()
 
-states = []
 
-for i in range(20):
+
+
+def forward(O):
+    T = O.shape[0]
+    a = np.zeros([N,T])
+
+    for j in range(N):
+        a[j,0] = P[j]*B[j,O[0]]
+
+    for t in range(1,T):
+        for j in range(N):
+            a[j,t] = 0
+            for i in range(N):
+                a[j,t] += a[i,t-1]*A[i,j]
+            a[j,t] *= B[j,O[t]]
+    
+    res = 0
+    for i in range(N):
+        res += a[i,T-1]
+
+    return res
+    
+
+states = []
+observations = []
+
+for i in range(1):
     if i==0:
         current_state = get_index(np.random.uniform(),P)[0]
     else:
         current_state = get_index(np.random.uniform(),A[current_state,:])[0]
     states.append(current_state)
     output = get_index(np.random.uniform(),B[current_state,:])[0]
+    observations.append(output)
     showNewRes(output)
     sleep(0.1)
-
+observations = np.array(observations)
+print('')
 print(states)
-
-
-
-
-    
-    
+print(observations)
+prob = forward(observations)
+print(f'probability = {prob}')
 
 
 
